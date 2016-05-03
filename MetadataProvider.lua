cef3e15d381ff0e1e6a7fc748c1cac2fb800552a
unknown <mikeyk730@gmail.com>
2016-05-03 00:36:48 -0400

    change label

--- MetadataProvider.lua
+++ MetadataProvider.lua
@@ -1,7 +1,7 @@
 local valuesFileStatus = {
    {
       value = 'loadedMetadata',
-      title = LOC '$$$/Metadata/Fields/FileStatus/MetadataLoaded=Cached in Catalog',
+      title = LOC '$$$/Metadata/Fields/FileStatus/MetadataLoaded= Metadata Cached',
    },
 }
 
